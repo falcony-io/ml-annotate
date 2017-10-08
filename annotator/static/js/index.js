c95fed46d98c1f229f3c6df5f74eeebb998f168b
Teemu Kokkonen <teemu.kokkonen@fastmonkeys.com>
2017-10-08 20:45:33 +0300

    Bring highlightify.js in

--- annotator/static/js/index.js
+++ annotator/static/js/index.js
@@ -1,7 +1,11 @@
 import bootstrap from 'bootstrap';
+import hljs from 'highlight.js';
 import renderPlot from './plot';
 import renderDataset from './dataset';
 import renderMultiLabelSelector from './multiLabelSelector';
+
+
+hljs.initHighlightingOnLoad();
 
 window.annotate = {
   renderPlot,
--- annotator/static/js/main.js
+++ annotator/static/js/main.js
@@ -1,4 +1,3 @@
-hljs.initHighlightingOnLoad();
 Mousetrap.bind('1', function() { document.getElementById('button1').click(); });
 Mousetrap.bind('2', function() { document.getElementById('button2').click(); });
 Mousetrap.bind('3', function() { document.getElementById('button3').click(); });
