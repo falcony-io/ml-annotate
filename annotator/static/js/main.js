fbfea5b424f9a9299066b423c567135bf1add9bf
Teemu Kokkonen <teemu.kokkonen@fastmonkeys.com>
2017-10-10 18:20:55 +0300

    Remove unneccessary file

--- annotator/static/js/main.js
+++
@@ -1,3 +0,0 @@
-Mousetrap.bind('1', function() { document.getElementById('button1').click(); });
-Mousetrap.bind('2', function() { document.getElementById('button2').click(); });
-Mousetrap.bind('3', function() { document.getElementById('button3').click(); });
