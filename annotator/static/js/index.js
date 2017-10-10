97161e3ac2cd7fff4657f08936b0b7c6fcc6be96
Teemu Kokkonen <teemu.kokkonen@fastmonkeys.com>
2017-10-10 11:07:27 +0300

    Bring Mousetrap in

--- annotator/static/js/index.js
+++ annotator/static/js/index.js
@@ -3,11 +3,15 @@ window.$ = $; // Thanks Bootstrap!
 
 import bootstrap from 'bootstrap';
 import hljs from 'highlight.js';
+import mousetrap from 'mousetrap';
 import renderPlot from './plot';
 import renderDataset from './dataset';
 import renderMultiLabelSelector from './multiLabelSelector';
 import renderProcessedJobs from './processedJobs';
 
+Mousetrap.bind('1', function() { document.getElementById('button1').click(); });
+Mousetrap.bind('2', function() { document.getElementById('button2').click(); });
+Mousetrap.bind('3', function() { document.getElementById('button3').click(); });
 hljs.initHighlightingOnLoad();
 window.annotate = {
   renderPlot,
