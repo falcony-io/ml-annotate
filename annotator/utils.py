08fd6804ba285aed785c4c17095bc4b2dd0c8183
Teemu Kokkonen <teemu.kokkonen@fastmonkeys.com>
2017-10-17 17:55:53 +0300

    Add 404 abort for non-existing problems

--- annotator/utils.py
+++ annotator/utils.py
@@ -3,6 +3,7 @@ from flask_login import current_user
 
 
 def assert_rights_to_problem(problem):
-    assert problem
+    if not problem:
+        abort(404)
     if not current_user.can_access_problem(problem):
         abort(403)
