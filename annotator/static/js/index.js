e286bc873263f7d69367958799f0e12736567702
Teemu Kokkonen <teemu.kokkonen@fastmonkeys.com>
2017-10-17 17:52:41 +0300

    Fix keybindings for binary classification

--- annotator/static/js/index.js
+++ annotator/static/js/index.js
@@ -10,12 +10,17 @@ import renderDataset from './dataset';
 import renderMultiLabelSelector from './multiLabelSelector';
 import renderProcessedJobs from './processedJobs';
 
-if (document.getElementById('button1')) {
-  Mousetrap.bind('1', function() { document.getElementById('button1').click(); });
-  Mousetrap.bind('2', function() { document.getElementById('button2').click(); });
-  Mousetrap.bind('3', function() { document.getElementById('button3').click(); });
-}
+$(() => {
+  if (document.getElementById('button1')) {
+    console.log('asd');
+    mousetrap.bind('1', function() { console.log('asd'); document.getElementById('button1').click(); });
+    mousetrap.bind('2', function() { document.getElementById('button2').click(); });
+    mousetrap.bind('3', function() { document.getElementById('button3').click(); });
+  }
+});
+
 hljs.initHighlightingOnLoad();
+
 window.annotate = {
   renderPlot,
   renderDataset,
